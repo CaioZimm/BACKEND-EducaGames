33b37efdf18c36c265265da4cbf32612b37992e7
Caio Claudino <caiovenancio06@gmail.com>
2025-04-11 15:05:57 -0300

    feat: add controllers in project

---
+++ app/Http/Controllers/FoundationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Foundation;
+use Illuminate\Http\Client\Request;
+
+class FoundationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Foundation $foundation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Foundation $foundation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Foundation $foundation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Foundation $foundation)
+    {
+        //
+    }
+}
