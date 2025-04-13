3b0be7a18d5fdf6134594868dda20b9783bce85b
CaioZimm <caiovenancio06@gmail.com>
2025-04-13 14:55:52 -0300

    feat: add functions about foundation controller

--- app/Http/Controllers/FoundationController.php
+++ app/Http/Controllers/FoundationController.php
@@ -3,63 +3,83 @@
 namespace App\Http\Controllers;
 
 use App\Models\Foundation;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
+use App\Http\Requests\FoundationRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class FoundationController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
+    public function index(){
+        if(!Foundation::first()){
+            return response()->json([ 'message' => "There aren't foundations" ], Response::HTTP_NOT_FOUND);
+        }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json( [ 'data' => Foundation::all() ], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(FoundationRequest $request){
+        $foundation = Foundation::create([
+            'name' => $request->name,
+            'description' => $request->description
+        ]);
+
+        return response()->json( [ 'message' => 'Foundation created successfully', 'data' => $foundation ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Foundation $foundation)
-    {
-        //
-    }
+    public function show($id){
+        $foundation = Foundation::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Foundation $foundation)
-    {
-        //
+        if(!$foundation){
+            return response()->json([ 'message' => 'Foundation not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json([ 'data' => $foundation ], Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Foundation $foundation)
-    {
-        //
+    public function update(Request $request, $id){
+        $request->validate([
+            'name' => ['nullable', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:255']
+        ]);
+        
+        $foundation = Foundation::find($id);
+
+        if(!$foundation){
+            return response()->json([ 'message' => 'Foundation not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        $foundation->update([
+            'name' => $request->name ?: $foundation->name,
+            'description' => $request->description ?: $foundation->description,
+        ]);
+
+        return response()->json( [ 'message' => 'Foundation updated successfully', 'data' => $foundation ], Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Foundation $foundation)
-    {
-        //
+    public function destroy($id){
+        $foundation = Foundation::find($id);
+
+        if(!$foundation){
+            return response()->json([ 'message' => 'Foundation not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        $foundation->delete();
+
+        return response()->json( [ 'message' => 'Foundation deleted successfully' ], Response::HTTP_OK);
     }
 }
