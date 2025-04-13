13afa65570f4b683c73062dae21a1991bbe6c191
CaioZimm <caiovenancio06@gmail.com>
2025-04-13 14:56:07 -0300

    feat: add game controller in project

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,62 +4,96 @@ namespace App\Http\Controllers;
 
 use App\Models\Game;
 use Illuminate\Http\Request;
+use App\Http\Requests\GameRequest;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class GameController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
+    public function index(){
+        if(!Game::first()){
+            return response()->json([ 'message' => "There aren't games" ], Response::HTTP_NOT_FOUND);
+        }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json( [ 'data' => Game::all() ], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(GameRequest $request){
+        $game = Game::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'mode' => $request->input('mode'),
+            'closed_time' => $request->closed_time,
+            'password' => $request->password,
+            'max_score' => $request->max_score,
+            'foundation_id' => Auth::user()?->foundation_id ?? $request->foundation_id,
+        ]);
+
+        return response()->json( [ 'message' => 'Game created successfully', 'data' => $game ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Game $game)
-    {
-        //
-    }
+    public function show($id){
+        $game = Game::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Game $game)
-    {
-        //
+        if(!$game){
+            return response()->json([ 'message' => 'Game not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json([ 'data' => $game ], Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Game $game)
-    {
-        //
+    public function update(Request $request, $id){
+        $request->validate([
+            'title' => ['nullable', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:255'],
+            'mode' => ['nullable', 'in:competitive,casual'],
+            'closed_time' => ['nullable', 'date'],
+            'password' => ['nullable', 'string'],
+            'max_score' => ['nullable'],
+        ]);
+
+        $game = Game::find($id);
+
+        if(!$game){
+            return response()->json([ 'message' => 'Game not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        $game->update([
+            'title' => $request->title ?: $game->title,
+            'description' => $request->description ?: $game->description,
+            'mode' => $request->mode ?: $game->mode,
+            'closed_time' => $request->closed_time ?: $game->closed_time,
+            'password' => $request->password ?: $game->password,
+            'max_score' => $request->max_score ?: $game->max_score
+        ]);
+
+        return response()->json( [ 'message' => 'Game updated successfully', 'data' => $game ], Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Game $game)
-    {
-        //
+    public function destroy($id){
+        $game = Game::find($id);
+
+        if(!$game){
+            return response()->json([ 'message' => 'Game not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        $game->delete();
+
+        return response()->json( [ 'message' => 'Game deleted successfully' ], Response::HTTP_OK);
     }
 }
---
+++ app/Http/Requests/GameRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GameRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:255'],
+            'mode' => ['in:competitive,casual'],
+            'closed_time' => ['nullable', 'date'],
+            'password' => ['nullable', 'string'],
+            'max_score' => ['nullable'],
+            'foundation_id' => ['required', 'exists:foundation,id']
+        ];
+    }
+}
