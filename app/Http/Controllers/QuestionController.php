94186fffaf53c438ac2882f04cdc017de88f2c86
Caio Claudino <caiovenancio06@gmail.com>
2025-04-14 14:53:04 -0300

    feat: add functions about question controller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,64 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\QuestionRequest;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class QuestionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
+    public function index(){
+        if(!Question::first()){
+            return response()->json([ 'message' => "There aren't questions" ], Response::HTTP_NOT_FOUND);
+        }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json(['data' => Question::all()], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(QuestionRequest $request){
+        $question = Question::create([
+            'description' => $request->description,
+            'type' => $request->input('type'),
+            'score' => $request->score,
+            'game_id' => $request->game_id
+        ]);
+        
+        return response()->json( [ 'message' => 'Question created successfully', 'data' => $question ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Question $question)
-    {
-        //
-    }
+    public function show($id){
+        $question = Question::with('alternative', 'answer')->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Question $question)
-    {
-        //
+        if(!$question){
+            return response()->json([ 'message' => 'Question not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json([ 'data' => $question ], Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Question $question)
-    {
-        //
+    public function update(Request $request, $id){
+        $request->validate([
+            'description' => ['string'],
+            'type' => ['in:open,mult'],
+            'score' => ['nullable']
+        ]);
+
+        $question = Question::find($id);
+
+        if(!$question){
+            return response()->json([ 'message' => 'Question not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        $question->update([
+            'description' => $request->description ?: $question->description,
+            'type' => $request->type ?: $question->type,
+            'score' => $request->score ?: $question->score
+        ]);
+
+        return response()->json( [ 'message' => 'Game updated successfully', 'data' => $question ], Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Question $question)
-    {
-        //
+    public function destroy($id){
+        $question = Question::find($id);
+
+        if(!$question){
+            return response()->json([ 'message' => 'Question not found' ], Response::HTTP_NOT_FOUND);
+        }
+
+        $question->delete();
+
+        return response()->json( [ 'message' => 'Question deleted successfully' ], Response::HTTP_OK);
     }
 }
